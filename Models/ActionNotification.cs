6f0abad2c209f90dddb31d48a3f35d2df320d6ec
douglasfarias <douglas185@live.com>
2020-11-29 11:56:47 -0300

    Atualização
    Adição do componente Selector para simplificar a manutenção dos seletores de cliente, parametros e os futuros.
    Correção da excessão que impedia adicionar uma mensagem ao banco de dados (A mensagem era adicionada ao mesmo tempo que o cliente).

--- Models/ActionNotification.cs
+++ Models/ActionNotification.cs
@@ -1,10 +1,13 @@
 using ActionNotifications.Models.Contracts;
+using System.Drawing;
+
 namespace ActionNotifications.Models
 {
     public class ActionNotification : IActionNotification
     {
         private string _Title;
         private string _Message;
+        private Color _TextColor;
 
         public string Title { 
             get {return _Title;} 
@@ -14,5 +17,10 @@ namespace ActionNotifications.Models
             get {return _Message;} 
             set {_Message = value;} 
             }
+
+        public Color TextColor {
+            get { return _TextColor; }
+            set { _TextColor = value; }
+        }
     }
 }
--- Models/Contracts/IActionNotification.cs
+++ Models/Contracts/IActionNotification.cs
@@ -1,8 +1,11 @@
+using System.Drawing;
+
 namespace ActionNotifications.Models.Contracts
 {
     public interface IActionNotification
     {
         string Title { get; set; }
         string Message { get; set; }
+        Color TextColor { get; set; }
     }
 }
